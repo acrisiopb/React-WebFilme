bc75336fa4d0fda1f5e41ac55845c6bbe977e108
Acrísio_Cruz <acrisiopb@gmail.com>
2025-07-01 10:21:56 -0300

    Star - implementação.

--- movie_app/src/components/StarRating/index.tsx
+++ movie_app/src/components/StarRating/index.tsx
@@ -1,14 +1,40 @@
+import { FaStar } from "react-icons/fa";
+import { FaRegStar } from "react-icons/fa";
+
 export interface Props {
     rating: number
 }
 
 export default function starRating(props: Props) {
+
     const numStarts = Math.round(props.rating / 2);
     // console.log(numStarts)
 
+    const fullStars = [];
+    const emptyStars = [];
+
+    for (let i = 0; i < 5; i++) {
+
+        if (i < numStarts) {
+            fullStars.push(i);
+        }
+        else {
+            emptyStars.push(i);
+        }
+    }
+
     return (
         <div>
-            Teste
+            
+            {fullStars.map(index =>
+                <FaStar key={index} />
+            )}
+            {emptyStars.map(index =>
+
+                <FaRegStar key={index}/>
+
+            )}
+
         </div>
     );
 
